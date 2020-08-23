8d95d043676589157b1201b51af4bee6767fc5f6
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-23 00:28:20 -0700

    updating products

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './Shop.css';
+
+import fakeData from '../../fakeData';
+
+
+
+const Shop = () => {
+    
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    return (
+        <div className="shop-container">
+            <div className="products-container">
+            <ul>
+                {
+                    products.map(product => <li>{product.name}</li>)
+                }
+            </ul>
+            </div>
+            <div className="cart-container">
+                <h2>this is container</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
